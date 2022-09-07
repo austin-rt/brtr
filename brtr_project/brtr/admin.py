fa568f87bf849d93a07b38ceec229e6fd75d9b35
austin-rt <austintaylorart@gmail.com>
2022-09-07 10:54:14 -0400

    NUKED IT ALL

--- brtr_project/brtr/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import DeliveryChoices, User, Category, Product, Order, UserReview, ProductReview
-admin.site.register(DeliveryChoices)
-# admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(UserReview)
-admin.site.register(ProductReview)
